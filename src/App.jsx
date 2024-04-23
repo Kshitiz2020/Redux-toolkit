f6f1afbcc7a2a9d9cecb88de6cb56e19e1223406
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-23 10:17:04 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Product from "./Components/Product";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Product.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+function Product() {
+  useEffect(() => {});
+  return (
+    <div>
+      <h1>Product Dashboard</h1>
+    </div>
+  );
+}
+export default Product;
