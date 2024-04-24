93c2ec3482ad43bc7132d1c085ef5d8e4c96f394
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-24 11:33:18 +0300

    routing-initated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Product from "./Components/Product";
+import Dashboard from "./Components/Dashboard";
+import Cart from "./Components/Cart";
+import RootLayout from "./Components/RootLayout";
 import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Dashboard />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+      </Route>
+    )
+  );
   return (
     <div>
-      <Product />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,41 @@
 import { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card"; // we need to import the react-bootstrap react component
+import Button from "react-bootstrap/Button"; // we need to import the react-bootstrap react component
 function Product() {
-  useEffect(() => {});
+  const [products, getProducts] = useState([]);
+  useEffect(() => {
+    // api calling
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => getProducts(result));
+  }, []);
+
+  //MAPPING json data
+
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <Card key={product.id} className="h-100">
+        <div className="text-center">
+          <Card.Img
+            variant="top"
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.price} €</Card.Text>
+        </Card.Body>
+        <Card.Footer style={{ background: "white" }}>
+          <Button variant="primary">Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
   return (
     <div>
       <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
     </div>
   );
 }
