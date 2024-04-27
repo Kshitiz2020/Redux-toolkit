1da05ed7f7155576ab9226c7ab69b00fdd327ae0
Kshitiz Khanal <103874414+Kshitiz2020@users.noreply.github.com>
2024-04-27 10:54:53 +0300

    synced code with redux-toolkit

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card"; // we need to import the react-bootstrap react component
 import Button from "react-bootstrap/Button"; // we need to import the react-bootstrap react component
+import StatusCode from "../utils/StatusCode";
 
 // importing reduxToolkit element
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { add } from "../store/CartSlice";
+
+// importing thunk product
+import { getProducts } from "../store/ProductSlice";
 
 function Product() {
   const dispatch = useDispatch(); // dispatching the action
 
-  const [products, getProducts] = useState([]);
+  const { data: products, status } = useSelector((state) => state.products);
+
+  //const [products, getProducts] = useState([]);
   useEffect(() => {
+    //dispatch an action for fetch products
+    dispatch(getProducts());
     // api calling
-    fetch("https://fakestoreapi.com/products")
+    /* fetch("https://fakestoreapi.com/products")
       .then((data) => data.json())
-      .then((result) => getProducts(result));
+      .then((result) => getProducts(result)); */
   }, []);
+
+  if (status === StatusCode.LOADING) {
+    return <p>Loading...</p>;
+  }
+  if (status === StatusCode.ERROR) {
+    return <p>you lost the way</p>;
+  }
 
   const addToCart = (product) => {
     //dispatch an add action
